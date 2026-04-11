a856570aac8892d56f0a03c369b73d034bff8f55
Aya Bean Jackson <pixelmixery@gmail.com>
2026-04-11 20:54:13 +1000

    progress?

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -3,34 +3,14 @@
 import re
 
 import datetime
-from dateutil.relativedelta import relativedelta
 import yaml
 
 from models.helper_models import DueDateTime
 
 DATETIME_FORMAT = r"%Y-%m-%dT%H:%M:%SZ"
 
-CONVERTER = {
-    "mon": 0,
-    "tue": 1,
-    "wed": 2,
-    "thu": 3,
-    "fri": 4,
-    "sat": 5,
-    "sun": 6,
-}
+PATTERN = r"(\d+)-(day|week|month|quarter|year)(.+)?"
 
-PATTERN = r"(\d+)-(day|week|month|quarter|year)(:?(?:mon|tue|wed|thu|fri|sat|sun|\d+]))?(@\d{1,4})?"
-
-DELTA_MAP = {
-    "day": lambda d, n: d + relativedelta(days=n),
-    "week": lambda d, n: d + relativedelta(weeks=n),
-    "month": lambda d, n, m=None: (
-        d + relativedelta(months=n, day=m) if m else d + relativedelta(months=n)
-    ),
-    "quarter": lambda d, n: d + relativedelta(months=n * 3),
-    "year": lambda d, n: d + relativedelta(years=n),
-}
 
 class Helper:
 
@@ -47,22 +27,6 @@ class Helper:
         """Builds deeplinks for link purposes"""
         return f"https://object.any.coop/{object_id}?spaceId={space_id}"
 
-    def date_eligibility(self, unit, modifier=None):
-        """Returns list of eligible values for days of the week"""
-
-        if unit in ["day", "week", "month", "quarter", "year"]:
-            allowed = [0, 1, 2, 3, 4, 5, 6]
-        elif unit == "week" and modifier:
-            allowed = [CONVERTER[d] for d in modifier.split(",")]
-        elif unit == "weekday":
-            allowed = [0, 1, 2, 3, 4]
-        elif unit == "weekend":
-            allowed = [5, 6]
-        else:
-            allowed = [CONVERTER.get(unit)]
-
-        return allowed
-
     def next_date(self, rate: str):
         """
         Returns formatted string of the next date based on the timescale provided
@@ -71,23 +35,19 @@ class Helper:
         days of the week - 1-week:mon,thu@14
         day of the month - 1-month:15
         """
-
         captured = re.search(PATTERN, rate).groups()
 
+        data_dict: dict = {"number": captured[0], "unit": captured[1]}
+        if captured[2]:
+            data_dict["extra"]: captured[2]
+
+        dt_next = DueDateTime(**data_dict)
+
         exit(0)
-        allowed = self.date_eligibility(unit, modifier)
-
-
-        dt_next = DELTA_MAP.get(unit, lambda d, n: d + relativedelta(days=n))(
-            self.get_today(), n
-        )
-
-        while dt_next.weekday() not in allowed:
-            dt_next += datetime.timedelta(days=1)
 
         return dt_next.strftime(DATETIME_FORMAT)
 
-    def get_today(self, replace = ["minute","hour"], string:bool = False):
+    def get_today(self, replace=["minute", "hour"], string: bool = False):
         """gets midnight today"""
 
         now = datetime.datetime.now().replace(microsecond=0, second=0)
